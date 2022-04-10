bfb76ade9e304902ffe0979a123af2db922c2344
Elina <papadelina145@gmail.com>
2022-04-10 19:23:08 +0300

    Initial commit.

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class Gameplay : MonoBehaviour
+{
+    private string correctPassword;
+    public List<TextMeshProUGUI> numberBoxes = new List<TextMeshProUGUI>();
+    public int currentNumBox;
+    private int isWrong = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        correctPassword = GetPassword();
+    }
+
+
+    string GetPassword()
+    {
+        string password = "";
+        for(int i=0; i<4; i++)
+        {
+            password = password + Random.Range(0, 10).ToString(); 
+        }
+        Debug.Log(password);
+        return password;
+    }
+
+    public void AddNumberToDisplay(string num)
+    {
+        if( numberBoxes[currentNumBox].text.Length < 4)
+        {
+            numberBoxes[currentNumBox].text += num.ToString();
+        }
+    }
+
+    public void CheckPassword()
+    {
+        string newText = "";
+        string oldText = numberBoxes[currentNumBox].text;
+        string tempPassword = correctPassword;
+        for(int i=0; i<4; i++)
+        {
+            string digit = numberBoxes[currentNumBox].text[i].ToString();
+            int frequency = 0;
+            int correctSpot = 0;
+
+
+            for(int j=0; j<4; j++)
+            {
+                if(digit == tempPassword[j].ToString())
+                {
+                    frequency++;
+                    if(i==j)
+                    {
+                        correctSpot = 1;
+                    }
+                }
+
+            }
+
+            if(frequency == 0)
+            {
+                newText += "<color=#A9A9A9>" + digit.ToString() + "</color>";
+            }
+            else if(correctSpot==1)
+            {
+                newText += "<color=#00FF00>" + digit.ToString() + "</color>";
+            }
+            else
+            {
+                newText += "<color=#FFA500>" + digit.ToString() + "</color>";
+            }
+
+        }
+
+        numberBoxes[currentNumBox].text = newText;
+
+        if(oldText == correctPassword)
+        {
+            isWrong = 0;
+            SceneManager.LoadScene("Win Screen");
+        }
+        else
+        {
+            isWrong = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentNumBox >= 5 && isWrong == 1)
+        {
+            SceneManager.LoadScene("Lose Screen");
+        }
+    }
+}
---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keyboard : MonoBehaviour
+{
+    public List<Button> numberKeys = new List<Button>();
+    public Button okKey;
+    public Button delKey;
+    private string numbers = "1234567890";
+    public Gameplay gameplay;
+
+
+    void Start()
+    {
+        SetupKeys();
+    }
+
+    void SetupKeys()
+    {
+        for(int i = 0; i < numberKeys.Count; i++)
+        {
+            numberKeys[i].transform.GetChild(0).GetComponent<Text>().text = numbers[i].ToString();
+        }
+
+        foreach (var key in numberKeys)
+        {
+            string number = key.transform.GetChild(0).GetComponent<Text>().text;
+            key.GetComponent<Button>().onClick.AddListener(() => ClickNumKey(number));
+        }
+
+        okKey.GetComponent<Button>().onClick.AddListener(() => ClickOkKey());
+        delKey.GetComponent<Button>().onClick.AddListener(() => ClickDelKey());
+    }
+
+    public void ClickOkKey()
+    {
+        if(gameplay.currentNumBox < 5 && gameplay.numberBoxes[gameplay.currentNumBox].text.Length == 4)
+        {
+            gameplay.CheckPassword();
+            gameplay.currentNumBox++;
+        }
+    }
+
+    public void ClickDelKey()
+    {
+        if(gameplay.numberBoxes[gameplay.currentNumBox].text.Length > 0)
+        {
+            string result = gameplay.numberBoxes[gameplay.currentNumBox].text.Remove(gameplay.numberBoxes[gameplay.currentNumBox].text.Length-1);
+            gameplay.numberBoxes[gameplay.currentNumBox].text = result;
+        }
+    }
+
+    public void ClickNumKey(string numKey)
+    {
+        // Debug.Log(numKey);
+        if(gameplay.currentNumBox < 5)
+        {
+            gameplay.AddNumberToDisplay(numKey);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
